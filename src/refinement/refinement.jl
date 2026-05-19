8f9b4f173ebe0f17a65524da29c604285518f94d
RubberLanding <nico.schumann@tutanota.com>
2026-05-19 22:31:47 +0200

    Change the property types of `ParticleRefinement` to be GPU-compatible. Add the smoothing length factor (Eq. 35 for the merging) to the refinement. Add `@adapt_structure ParticleRefinement`.

--- src/refinement/refinement.jl
+++ src/refinement/refinement.jl
@@ -1,31 +1,36 @@
 include("spacing.jl")
 include("refinement_criteria.jl")
-struct ParticleRefinement{RC, ELTYPE, SP}
-    criteria            :: RC             # Tuple of all refinement criteria to be applied, e.g. `SpatialRefinementCriterion` and `SolutionRefinementCriterion`` 
-    max_spacing_ratio   :: ELTYPE         # Ratio between spacings of different refinement bands, should be between 1.05 and 1.20
-    min_spacing         :: ELTYPE         # The minimum spacing being used in either boundaries or solids
-    splitting_pattern   :: SP
-    split_candidates    :: Vector{Int}
-    merge_candidates    :: Vector{Int}
-    delete_candidates   :: Vector{Bool}
-    n_new_particles     :: Ref{Int}
-    n_current_particles :: Ref{Int}
+struct ParticleRefinement{RC, ELTYPE, SP, BARRAY, IARRAY}
+    criteria                :: RC        # Tuple of all refinement criteria to be applied, e.g. `SpatialRefinementCriterion` and `SolutionRefinementCriterion`` 
+    max_spacing_ratio       :: ELTYPE    # Ratio between spacings of different refinement bands, should be between 1.05 and 1.20     
+    min_spacing             :: ELTYPE    # The minimum spacing being used in either boundaries or solids  
+    smoothing_length_factor :: ELTYPE    # Constant corresponding to the value of smoothing length factor used in the simulation, see Eq. 35 for the merging proceduce
+    splitting_pattern       :: SP
+    delete_candidates       :: BARRAY  
+    split_candidates        :: IARRAY  
+    merge_candidates        :: IARRAY  
+    n_new_particles         :: IARRAY 
+    n_current_particles     :: IARRAY 
 end
 
 function ParticleRefinement(; n_particles, smoothing_length, initial_particle_spacing,
-                            max_spacing_ratio, min_spacing,
+                            max_spacing_ratio, min_spacing, smoothing_length_factor = 1.2,
                             refinement_criteria=SpatialRefinementCriterion(),
                             splitting_pattern=nothing)
     if !(refinement_criteria isa Tuple)
         refinement_criteria = (refinement_criteria,)
     end
 
+    delete_candidates = Vector{Bool}(undef, n_particles)
     split_candidates = Vector{Int}(undef, n_particles)
     merge_candidates = Vector{Int}(undef, n_particles)
-    delete_candidates = Vector{Bool}(undef, n_particles)
 
-    return ParticleRefinement(refinement_criteria, max_spacing_ratio, min_spacing,
-                              splitting_pattern, split_candidates, merge_candidates, delete_candidates, Ref(0), Ref(0))
+    n_new_particles = zeros(Int, 1)
+    n_current_particles = zeros(Int, 1)
+
+    return ParticleRefinement(refinement_criteria, max_spacing_ratio, min_spacing, smoothing_length_factor,
+                              splitting_pattern, split_candidates, merge_candidates, 
+                              delete_candidates, n_new_particles, n_current_particles)
 end
 
 # TODO:
